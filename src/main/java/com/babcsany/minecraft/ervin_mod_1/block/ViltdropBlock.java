ca88a9dd7b560c6bca45d8b09e456b414a9bee29
Ervin Babcsány <ervin@babcsany.com>
2020-04-18 20:47:11 +0200

    Initial commit

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/ViltdropBlock.java
@@ -0,0 +1,9 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class ViltdropBlock extends Block {
+    public ViltdropBlock(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/init/BlockInit.java
@@ -0,0 +1,22 @@
+package com.babcsany.minecraft.ervin_mod_1.init;
+
+import com.babcsany.minecraft.ervin_mod_1.Ervin_mod_1;
+import com.babcsany.minecraft.ervin_mod_1.block.ViltdropBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BlockInit {
+
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Ervin_mod_1.MOD_ID);
+
+    public static final RegistryObject<Block> VILTDROP = BLOCKS.register("viltdrop", () -> new ViltdropBlock(
+            Block.Properties.create(Material.ROCK)
+                    .harvestLevel(3)
+                    .harvestTool(ToolType.PICKAXE)
+                    .hardnessAndResistance(50.0f)
+    ));
+}
