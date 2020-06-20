1430d14a2a23dfc83a36d6b8b84ddb4de20b1e21
Ervin Babcsány <ervin@babcsany.com>
2020-06-20 15:22:59 +0200

    added NetherPortal.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/NetherPortal.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class NetherPortal extends Block {
+    public NetherPortal(Block.Properties properties) { super(properties); }
+}
