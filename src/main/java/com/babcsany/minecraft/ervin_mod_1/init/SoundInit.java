cdcca4a768fbcf910af06f9d5ef68a61203429b3
Ervin Babcsány <ervin@babcsany.com>
2020-06-20 12:58:58 +0200

    block init, recipes and tags changes, added missing files

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/init/SoundInit.java
@@ -0,0 +1,11 @@
+package com.babcsany.minecraft.ervin_mod_1.init;
+
+import net.minecraft.util.SoundEvent;
+
+public class SoundInit {
+    public static class AMBIENT {
+        public static SoundEvent get() {
+            return null;
+        }
+    }
+}
