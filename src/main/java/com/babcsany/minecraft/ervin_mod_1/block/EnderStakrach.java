efa8deb8190b6c957ea100f177df5cffed3a0b2a
Ervin Babcsány <ervin@babcsany.com>
2020-06-22 20:26:41 +0200

    added EnderSrackth.java, EnderStakrach.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/EnderStakrach.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class EnderStakrach extends Block {
+    public EnderStakrach(Block.Properties properties) { super(properties); }
+}
