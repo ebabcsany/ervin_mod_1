d7f788ba5979e681f7d3128653fcf23a869d178b
Ervin Babcsány <ervin@babcsany.com>
2020-06-24 13:23:17 +0200

    added EnderTrask.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/EnderTrask.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class EnderTrask extends Block {
+    public EnderTrask(Block.Properties properties) { super(properties); }
+}
