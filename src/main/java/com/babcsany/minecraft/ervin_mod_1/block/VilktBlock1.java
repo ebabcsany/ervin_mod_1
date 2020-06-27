ef197e3ef8bd508de14ac9d78c9298478b2564ea
Ervin Babcsány <ervin@babcsany.com>
2020-06-27 14:03:12 +0200

    added VilktBlock1.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/VilktBlock1.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class VilktBlock1 extends Block {
+    public VilktBlock1(Block.Properties properties) { super(properties); }
+}
