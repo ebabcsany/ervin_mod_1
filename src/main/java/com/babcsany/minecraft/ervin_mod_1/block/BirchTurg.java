3069ffb7404832e409502611a05bb6ea48d477d7
Ervin Babcsány <ervin@babcsany.com>
2020-07-01 07:05:52 +0200

    added AcaciaTurg, BirchTurg, DarkOakTurg, JungleTurg, SpruceTurg

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/BirchTurg.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class BirchTurg extends Block {
+    public BirchTurg(Block.Properties properties) { super(properties); }
+}
