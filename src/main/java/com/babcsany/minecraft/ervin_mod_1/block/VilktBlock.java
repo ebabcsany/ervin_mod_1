6a322fe9550ef6615a3e4e371c2a0416fe92bd31
Ervin Babcsány <ervin@babcsany.com>
2020-07-02 22:11:49 +0200

    modified VilktBlock.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/block/VilktBlock.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/VilktBlock.java
@@ -1,7 +1,18 @@
 package com.babcsany.minecraft.ervin_mod_1.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.properties.DoorHingeSide;
+import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.util.Direction;
+
+import static net.minecraft.block.TripWireHookBlock.FACING;
 
 public class VilktBlock extends Block {
-    public VilktBlock(Block.Properties properties) { super(properties); }
+    //public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    public VilktBlock(Block.Properties properties) {
+        super(properties);
+        //this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH)); //Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.DOWN, Direction.UP));
+    }
 }
