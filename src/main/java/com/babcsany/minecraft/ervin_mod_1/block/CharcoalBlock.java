3c4aeb22477470f7b4a157653c0b38579933e93d
Ervin Babcsány <ervin@babcsany.com>
2020-07-03 17:19:59 +0200

    added CharcoalBlock.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/CharcoalBlock.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class CharcoalBlock extends Block {
+    public CharcoalBlock(Block.Properties properties) { super(properties); }
+}
