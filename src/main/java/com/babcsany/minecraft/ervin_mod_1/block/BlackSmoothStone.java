662bf5ed8b4b4e9dd8e2504755eb148a3c938ef7
Ervin Babcsány <ervin@babcsany.com>
2020-07-25 14:38:48 +0200

    added BlackCobblestoneStairs.java, BlackSmoothStone.java, BlackSmoothStoneSlab.java, BlackSmoothStoneStairs.java, BlackStoneStairs.java, BlueCobblestoneStairs.java, BlueSmoothStone.java, BlueSmoothStoneSlab.java, BlueSmoothStoneStairs.java, BlueStoneStairs.java, BrownCobblestoneStairs.java, BrownSmoothStone.java, BrownSmoothStoneSlab.java, BrownSmoothStoneStairs.java, BrownStoneStairs.java, CyanCobblestoneStairs.java, CyanSmoothStone.java, CyanSmoothStoneSlab.java, CyanSmoothStoneStairs.java, CyanStoneStairs.java, GreenCobblestoneStairs.java, GreenSmoothStone.java, GreenSmoothStoneSlab.java, GreenSmoothStoneStairs.java, GreenStoneStairs.java, LightBlueCobblestoneStairs.java, LightBlueSmoothStone.java, LightBlueSmoothStoneSlab.java, LightBlueSmoothStoneStairs.java, LightBlueStoneStairs.java, LightGrayCobblestoneStairs.java, LightGraySmoothStone.java, LightGraySmoothStoneSlab.java, LightGraySmoothStoneStairs.java, LightGrayStoneStairs.java, LimeCobblestoneStairs.java, LimeSmoothStone.java, LimeSmoothStoneSlab.java, LimeSmoothStoneStairs.java, LimeStoneStairs.java, MagentaCobblestoneStairs.java, MagentaSmoothStone.java, MagentaSmoothStoneSlab.java, MagentaSmoothStoneStairs.java, MagentaStoneStairs.java, OrangeCobblestoneStairs.java, OrangeSmoothStone.java, OrangeSmoothStoneSlab.java, OrangeSmoothStoneStairs.java, OrangeStoneStairs.java, PinkCobblestoneStairs.java, PinkSmoothStone.java, PinkSmoothStoneSlab.java, PinkSmoothStoneStairs.java, PinkStoneStairs.java, PurpleCobblestoneStairs.java, PurpleSmoothStone.java, PurpleSmoothStoneSlab.java, PurpleSmoothStoneStairs.java, PurpleStoneStairs.java, RedCobblestoneStairs.java, RedSmoothStone.java, RedSmoothStoneSlab.java, RedSmoothStoneStairs.java, RedStoneStairs.java, WhiteCobblestoneStairs.java, WhiteSmoothStone.java, WhiteSmoothStoneSlab.java, WhiteSmoothStoneStairs.java, WhiteStoneStairs.java, YellowCobblestoneStairs.java, YellowSmoothStone.java, YellowSmoothStoneSlab.java, YellowSmoothStoneStairs.java, YellowStoneStairs.java,

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/BlackSmoothStone.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class BlackSmoothStone extends Block {
+    public BlackSmoothStone(Block.Properties properties) { super(properties); }
+}
---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/PurpleSmoothStone.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class PurpleSmoothStone extends Block {
+    public PurpleSmoothStone(Block.Properties properties) { super(properties); }
+}
