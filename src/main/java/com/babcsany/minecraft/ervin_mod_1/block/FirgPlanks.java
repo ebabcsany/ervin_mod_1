1913b3f55abeaa9160e7209301542e5a5db611a3
Ervin Babcsány <ervin@babcsany.com>
2020-08-03 09:37:43 +0200

    added ervin_mod_1/block, Durt.java, EnderTrask1.java, FirgPlanks.java, FirgSlab.java, FirgStairs.java, FirgWood.java, GankSlab.java, GritkBlock.java, GritkBlock1.java, RugkBlock.java, StrippedFirgLog.java, StrippedFirgWood.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/FirgPlanks.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class FirgPlanks extends Block{
+    public FirgPlanks(Block.Properties properties) { super(properties); }
+}
