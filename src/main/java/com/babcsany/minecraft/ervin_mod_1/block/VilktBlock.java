b64999765ca49e266f880feea36e9d1faee76f24
Ervin Babcsány <ervin@babcsany.com>
2020-08-06 07:55:17 +0200

    modified ervin_mod_1/block, VilktBlock.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/block/VilktBlock.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/VilktBlock.java
@@ -1,18 +1,21 @@
 package com.babcsany.minecraft.ervin_mod_1.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.properties.DoorHingeSide;
-import net.minecraft.state.properties.DoubleBlockHalf;
-import net.minecraft.util.Direction;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
 
-import static net.minecraft.block.TripWireHookBlock.FACING;
+public class VilktBlock extends HorizontalBlock {
+   public VilktBlock(Properties properties) {
+      super(properties);
+   }
 
-public class VilktBlock extends Block {
-    //public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
-    public VilktBlock(Block.Properties properties) {
-        super(properties);
-        //this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH)); //Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.DOWN, Direction.UP));
-    }
+   protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+      builder.add(HORIZONTAL_FACING);
+   }
+
+   public BlockState getStateForPlacement(BlockItemUseContext context) {
+      return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
+   }
 }
