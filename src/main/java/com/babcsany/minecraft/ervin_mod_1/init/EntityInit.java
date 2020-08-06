b6f8013244dc16a7720e9423ca4455f5f1b770d2
Ervin Babcsány <ervin@babcsany.com>
2020-08-06 10:12:13 +0200

    modified init, EntityInit.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/init/EntityInit.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/init/EntityInit.java
@@ -1,10 +1,14 @@
 package com.babcsany.minecraft.ervin_mod_1.init;
 
 import com.babcsany.minecraft.ervin_mod_1.Ervin_mod_1;
+//import com.babcsany.minecraft.ervin_mod_1.entity.animal.FreinEntity;
 import com.babcsany.minecraft.ervin_mod_1.entity.animal.FreinEntity;
 import com.babcsany.minecraft.ervin_mod_1.entity.animal.ViltEntity;
 import com.babcsany.minecraft.ervin_mod_1.entity.animal.ZurEntity1;
+import com.babcsany.minecraft.ervin_mod_1.entity.item.FirgBoatEntity;
 import com.babcsany.minecraft.ervin_mod_1.entity.monster.ZurEntity;
+import com.babcsany.minecraft.ervin_mod_1.entity.projectile.ScrakthsPearlEntity;
+import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.ResourceLocation;
@@ -21,22 +25,20 @@ public class EntityInit {
             .register("zur_entity_1",
                     () -> EntityType.Builder.<ZurEntity1>create(ZurEntity1::new, EntityClassification.CREATURE)
                             .size(0.9f, 1.3f)
-                            .build(new ResourceLocation(Ervin_mod_1.MOD_ID, "zur_entity_1").toString()));
-
+                            .build(new ResourceLocation(Ervin_mod_1.MOD_ID, "zur_entity_1").toString()
+                            ));
     public static final RegistryObject<EntityType<ZurEntity>> ZUR_ENTITY = ENTITY_TYPES
             .register("zur_entity",
                     () -> EntityType.Builder.<ZurEntity>create(ZurEntity::new, EntityClassification.MONSTER)
                             .size(0.6f, 1.95f)
-                            .build(new ResourceLocation(Ervin_mod_1.MOD_ID, "zur_entity").toString()));
-
-
+                            .build(new ResourceLocation(Ervin_mod_1.MOD_ID, "zur_entity").toString()
+                            ));
     public static final RegistryObject<EntityType<FreinEntity>> FREIN_ENTITY = ENTITY_TYPES
             .register("frein_entity",
-                    () -> EntityType.Builder.<FreinEntity>create(FreinEntity::new, EntityClassification.MONSTER)
-                            .size(0.6f, 1.95f)
-                            .build(new ResourceLocation(Ervin_mod_1.MOD_ID, "frein_entity").toString()));
-
-
+                    () -> EntityType.Builder.<FreinEntity>create(FreinEntity::new, EntityClassification.CREATURE)
+                            .size(1.0f, 3.0f)
+                            .build(new ResourceLocation(Ervin_mod_1.MOD_ID, "frein_entity").toString()
+                            ));
     public static final RegistryObject<EntityType<ViltEntity>> VILT_ENTITY = ENTITY_TYPES
             .register("vilt_entity",
                     () -> EntityType.Builder.<ViltEntity>create(ViltEntity::new, EntityClassification.CREATURE)
