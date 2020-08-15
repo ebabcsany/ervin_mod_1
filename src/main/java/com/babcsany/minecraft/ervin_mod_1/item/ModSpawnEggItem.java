2c584e2503f286d1b226164df65af23330eeaf35
Ervin Babcsány <ervin@babcsany.com>
2020-08-15 11:49:10 +0200

    2 modified ervin_mod_1/item, GretItem.java, ModSpawnEggItem.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/item/ModSpawnEggItem.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/item/ModSpawnEggItem.java
@@ -5,6 +5,7 @@ import net.minecraft.dispenser.DefaultDispenseItemBehavior;
 import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.CompoundNBT;
@@ -33,49 +34,47 @@ import java.util.Properties;
  */
 public class ModSpawnEggItem extends SpawnEggItem {
 
-    protected static final List<ModSpawnEggItem> UNADDED_EGGS = new ArrayList<>();
+    protected static final List<ModSpawnEggItem> UNADDED_EGGS = new ArrayList<ModSpawnEggItem>();
     private final Lazy<? extends EntityType<?>> entityTypeSupplier;
 
-    public ModSpawnEggItem(final NonNullSupplier<? extends EntityType<?>> entityTypeSupplier, final int p_i48465_2_, final int p_i48465_3_, final Properties p_i48465_4_) {
-        super(null, p_i48465_2_, p_i48465_3_, p_i48465_4_);
+    public ModSpawnEggItem(final NonNullSupplier<? extends EntityType<?>> entityTypeSupplier, final int primaryColour,
+                           final int secondaryColour, final Item.Properties properties) {
+        super(null, primaryColour, secondaryColour, properties);
         this.entityTypeSupplier = Lazy.of(entityTypeSupplier::get);
         UNADDED_EGGS.add(this);
     }
 
-    public ModSpawnEggItem(final RegistryObject<? extends EntityType<?>> entityTypeSupplier, final int p_i48465_2_, final int p_i48465_3_, final Properties p_i48465_4_) {
-        super(null, p_i48465_2_, p_i48465_3_, p_i48465_4_);
-        this.entityTypeSupplier = Lazy.of(entityTypeSupplier);
+    public ModSpawnEggItem(final RegistryObject<? extends EntityType<?>> entityTypeSupplier, final int primaryColour,
+                           final int secondaryColour, final Item.Properties properties) {
+        super(null, primaryColour, secondaryColour, properties);
+        this.entityTypeSupplier = Lazy.of(entityTypeSupplier::get);
         UNADDED_EGGS.add(this);
     }
 
-    /**
-     * Adds all the supplier based spawn eggs to vanilla's map and registers an
-     * IDispenseItemBehavior for each of them as normal spawn eggs have one
-     * registered for each of them during {@link net.minecraft.dispenser.IDispenseItemBehavior#init()}
-     * but supplier based ones won't have had their EntityTypes created yet.
-     */
-    public static void initUnaddedEggs() {
-        final Map<EntityType<?>, SpawnEggItem> EGGS = ObfuscationReflectionHelper.getPrivateValue(SpawnEggItem.class, null, "field_195987_b");
-        DefaultDispenseItemBehavior defaultDispenseItemBehavior = new DefaultDispenseItemBehavior() {
-            public ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
+    public static void initSpawnEggs() {
+        final Map<EntityType<?>, SpawnEggItem> EGGS = ObfuscationReflectionHelper.getPrivateValue(SpawnEggItem.class,
+                null, "field_195987_b");
+        DefaultDispenseItemBehavior dispenseBehaviour = new DefaultDispenseItemBehavior() {
+            @Override
+            protected ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
                 Direction direction = source.getBlockState().get(DispenserBlock.FACING);
-                EntityType<?> entitytype = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
-                entitytype.spawn(source.getWorld(), stack, null, source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
+                EntityType<?> type = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
+                type.spawn(source.getWorld(), stack, null, source.getBlockPos().offset(direction),
+                        SpawnReason.DISPENSER, direction != Direction.UP, false);
                 stack.shrink(1);
                 return stack;
             }
         };
-        for (final SpawnEggItem egg : UNADDED_EGGS) {
-            EGGS.put(egg.getType(null), egg);
-            DispenserBlock.registerDispenseBehavior(egg, defaultDispenseItemBehavior);
-            // ItemColors for each spawn egg don't need to be registered because this method is called before ItemColors is created
+
+        for (final SpawnEggItem spawnEgg : UNADDED_EGGS) {
+            EGGS.put(spawnEgg.getType(null), spawnEgg);
+            DispenserBlock.registerDispenseBehavior(spawnEgg, dispenseBehaviour);
         }
         UNADDED_EGGS.clear();
     }
 
     @Override
-    public EntityType<?> getType(@Nullable final CompoundNBT p_208076_1_) {
-        return entityTypeSupplier.get();
+    public EntityType<?> getType(CompoundNBT nbt) {
+        return this.entityTypeSupplier.get();
     }
-
 }
