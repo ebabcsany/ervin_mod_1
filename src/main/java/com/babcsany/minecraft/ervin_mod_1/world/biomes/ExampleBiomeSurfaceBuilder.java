7f5b1c8cbd911adaf5a0c230abdd524a114f9f4b
Ervin Babcsány <ervin@babcsany.com>
2020-08-15 11:55:54 +0200

    3 added ervin_mod_1/world/biomes, ExampleBiome.java, ExampleBiomeSurfaceBuilder.java, MigBiomeSurfaceBuilder.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/world/biomes/ExampleBiomeSurfaceBuilder.java
@@ -0,0 +1,40 @@
+package com.babcsany.minecraft.ervin_mod_1.world.biomes;
+
+import com.babcsany.minecraft.ervin_mod_1.init.BlockInit;
+import com.mojang.datafixers.Dynamic;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+
+import java.util.Random;
+import java.util.function.Function;
+
+public class ExampleBiomeSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
+
+	public ExampleBiomeSurfaceBuilder(Function<Dynamic<?>, ? extends SurfaceBuilderConfig> function) {
+		super(function);
+	}
+
+	@Override
+	public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise,
+			BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
+		Random rd = new Random();
+		int i = rd.nextInt(3);
+		if (i == 0) {
+			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
+					defaultFluid, seaLevel, seed,
+					new SurfaceBuilderConfig(BlockInit.EXAMPLE_BLOCK.get().getDefaultState(),
+							BlockInit.DEF_BLOCK.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
+		} else {
+			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
+					defaultFluid, seaLevel, seed,
+					new SurfaceBuilderConfig(
+							i == 1 ? Blocks.GRASS_BLOCK.getDefaultState()
+									: BlockInit.EXAMPLE_BLOCK.get().getDefaultState(),
+							BlockInit.DEF_BLOCK.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
+		}
+	}
+}
