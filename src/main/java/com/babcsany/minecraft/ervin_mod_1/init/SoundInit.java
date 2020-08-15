3fb3dc98e04566052710f172cea8da2e1ac7bcbe
Ervin Babcsány <ervin@babcsany.com>
2020-08-15 11:39:19 +0200

    1 modified ervin_mod_1/init, SoundInit.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/init/SoundInit.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/init/SoundInit.java
@@ -1,8 +1,13 @@
 package com.babcsany.minecraft.ervin_mod_1.init;
 
+import com.babcsany.minecraft.ervin_mod_1.Ervin_mod_1;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class SoundInit {
+
+    public static final DeferredRegister<SoundEvent> SOUNDS = new DeferredRegister<>(ForgeRegistries.SOUND_EVENTS, Ervin_mod_1.MOD_ID);
     public static class AMBIENT {
         public static SoundEvent get() {
             return null;
