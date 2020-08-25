c99ba3b1587d81dea8ace2897d8190dd00f33cb3
Ervin Babcsány <ervin@babcsany.com>
2020-08-25 16:53:10 +0200

    2 modified ervin_mod_1/world/biomes, ExampleBiome.java, ExampleBiomeSurfaceBuilder.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/world/biomes/ExampleBiomeSurfaceBuilder.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/world/biomes/ExampleBiomeSurfaceBuilder.java
@@ -1,7 +1,7 @@
 package com.babcsany.minecraft.ervin_mod_1.world.biomes;
 
-import com.babcsany.minecraft.ervin_mod_1.init.BlockInit;
 import com.mojang.datafixers.Dynamic;
+import com.babcsany.minecraft.ervin_mod_1.init.BlockInit;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.Biome;
@@ -22,19 +22,19 @@ public class ExampleBiomeSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderCon
 	public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise,
 			BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
 		Random rd = new Random();
-		int i = rd.nextInt(3);
+		int i = rd.nextInt(14);
 		if (i == 0) {
 			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
 					defaultFluid, seaLevel, seed,
-					new SurfaceBuilderConfig(BlockInit.EXAMPLE_BLOCK.get().getDefaultState(),
-							BlockInit.DEF_BLOCK.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
+					new SurfaceBuilderConfig(BlockInit.LEAT_BLOCK_CRAFTING_TABLE.get().getDefaultState(),
+							BlockInit.TRINKS.get().getDefaultState(), BlockInit.GRITK_BLOCK.get().getDefaultState()));
 		} else {
 			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
 					defaultFluid, seaLevel, seed,
 					new SurfaceBuilderConfig(
 							i == 1 ? Blocks.GRASS_BLOCK.getDefaultState()
-									: BlockInit.EXAMPLE_BLOCK.get().getDefaultState(),
-							BlockInit.DEF_BLOCK.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
+									: BlockInit.SRIUNK_BLOCK.get().getDefaultState(),
+							BlockInit.TRUGN.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
 		}
 	}
 }
