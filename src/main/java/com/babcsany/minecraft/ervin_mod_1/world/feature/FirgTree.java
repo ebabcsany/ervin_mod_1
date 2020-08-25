a461159cb49304cfe359f2b436854549d38b63af
Ervin Babcsány <ervin@babcsany.com>
2020-08-25 16:56:49 +0200

    1 modified ervin_mod_1/world/feature, FirgTree.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/world/feature/FirgTree.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/world/feature/FirgTree.java
@@ -1,6 +1,7 @@
 package com.babcsany.minecraft.ervin_mod_1.world.feature;
 
 import com.babcsany.minecraft.ervin_mod_1.init.BlockInit;
+import net.minecraft.block.trees.Tree;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
@@ -10,15 +11,17 @@ import net.minecraftforge.common.IPlantable;
 
 import java.util.Random;
 
-public class FirgTree {
+import static com.babcsany.minecraft.ervin_mod_1.world.feature.ModDefaultBiomeFeatures.FIRG_TREE_CONFIG;
+
+public class FirgTree extends Tree {
 
     public static final TreeFeatureConfig FIRG_TREE_CONFIG = (new TreeFeatureConfig.Builder(
             new SimpleBlockStateProvider(BlockInit.FIRG_LOG.get().getDefaultState()),
             new SimpleBlockStateProvider(BlockInit.FIRG_LEAVES.get().getDefaultState()),
-            new BlobFoliagePlacer(10, 10))).baseHeight(40).heightRandA(5).foliageHeight(125).ignoreVines()
+            new BlobFoliagePlacer(10, 4))).baseHeight(15).heightRandA(30).foliageHeight(25).ignoreVines()
             .setSapling((IPlantable) BlockInit.FIRG_SAPLING.get()).build();
 
-    //@Override
+    @Override
     protected ConfiguredFeature<TreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean b) {
         return Feature.NORMAL_TREE.withConfiguration(FIRG_TREE_CONFIG);
     }
