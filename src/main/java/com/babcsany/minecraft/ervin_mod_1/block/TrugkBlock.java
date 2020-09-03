c445cf377570a89cc796777886cfa7014dc408b8
Ervin Babcsány <ervin@babcsany.com>
2020-09-03 16:42:38 +0200

    8 added ervin_mod_1/block, ExamplePortalBlock.java, FlowingFluidBlock1.java, FlowingFluidBlock4.java, GrithBlock.java, IForgeBlock1.java, IForgeBlockState1.java, RedGrassBlock.java, TrugkBlock.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/TrugkBlock.java
@@ -0,0 +1,7 @@
+package com.babcsany.minecraft.ervin_mod_1.block;
+
+import net.minecraft.block.Block;
+
+public class TrugkBlock extends Block {
+    public TrugkBlock(Properties properties) { super(properties); }
+}
