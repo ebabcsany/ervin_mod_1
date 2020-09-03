c7ddfacf4690af9934274c71af38edc707b8e0e2
Ervin Babcsány <ervin@babcsany.com>
2020-09-03 17:12:55 +0200

    2 added ervin_mod_1/world/gen, GenerationStage1.java, TutorialOreGen.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/world/gen/TutorialOreGen.java
@@ -0,0 +1,84 @@
+package com.babcsany.minecraft.ervin_mod_1.world.gen;
+
+import com.babcsany.minecraft.ervin_mod_1.init.BlockInit;
+import com.babcsany.minecraft.ervin_mod_1.world.feature.OreFeatureConfig1;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TutorialOreGen {
+	public static void generateOre() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			if (biome == Biomes.PLAINS) {
+				ConfiguredPlacement customConfig = Placement.COUNT_RANGE
+						.configure(new CountRangeConfig(20, 5, 5, 25));
+				biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.ORE
+					.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.SURVI.get().getDefaultState(), 74))
+					.withPlacement(customConfig));
+			}
+		}
+	}
+	public static void generateBlackStone() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			if (biome == Biomes.PLAINS) {
+				ConfiguredPlacement customConfig = Placement.COUNT_RANGE
+						.configure(new CountRangeConfig(10, 15, 15, 20));
+				biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.ORE
+						.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, BlockInit.BLACK_STONE.get().getDefaultState(), 24))
+						.withPlacement(customConfig));
+			}
+		}
+	}
+	public static void generateBlueStone() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			if (biome == Biomes.PLAINS) {
+				ConfiguredPlacement customConfig = Placement.COUNT_RANGE
+						.configure(new CountRangeConfig(10, 15, 15, 20));
+				biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.ORE
+						.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, BlockInit.BLUE_STONE.get().getDefaultState(), 24))
+						.withPlacement(customConfig));
+			}
+		}
+	}
+	public static void generateBrownStone() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			if (biome == Biomes.PLAINS) {
+				ConfiguredPlacement customConfig = Placement.COUNT_RANGE
+						.configure(new CountRangeConfig(10, 15, 15, 20));
+				biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.ORE
+						.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, BlockInit.BROWN_STONE.get().getDefaultState(), 24))
+						.withPlacement(customConfig));
+			}
+		}
+	}
+	public static void generateCyanStone() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			if (biome == Biomes.PLAINS) {
+				ConfiguredPlacement customConfig = Placement.COUNT_RANGE
+						.configure(new CountRangeConfig(10, 15, 15, 20));
+				biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.ORE
+						.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, BlockInit.CYAN_STONE.get().getDefaultState(), 24))
+						.withPlacement(customConfig));
+			}
+		}
+	}
+	public static void generateGreenStone() {
+		for (Biome biome : ForgeRegistries.BIOMES) {
+			if (biome == Biomes.PLAINS) {
+				ConfiguredPlacement customConfig = Placement.COUNT_RANGE
+						.configure(new CountRangeConfig(10, 15, 15, 20));
+				biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.ORE
+						.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, BlockInit.GREEN_STONE.get().getDefaultState(), 24))
+						.withPlacement(customConfig));
+			}
+		}
+	}
+}
+
+//OreFeatureConfig.FillerBlockType.create("end_stone", null, new BlockMatcher(Blocks.END_STONE))
