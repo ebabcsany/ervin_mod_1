741a6973b273db5ff1a25703858ab1c97b840c93
Ervin Babcsány <ervin@babcsany.com>
2020-09-03 12:54:56 +0200

    13 added ervin_mod_1/world/biomes, Biome1.java, ExampleBiome0.java, ExampleBiome1.java, ExampleBiome2.java, ExampleBiome3.java, ExampleBiome4.java, ExampleBiome5.java, ExampleBiomeSurfaceBuilder0.java, ExampleBiomeSurfaceBuilder1.java, ExampleBiomeSurfaceBuilder2.java, ExampleBiomeSurfaceBuilder3.java, ExampleBiomeSurfaceBuilder4.java, OreBlock1.java

---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/world/biomes/ExampleBiome2.java
@@ -0,0 +1,111 @@
+package com.babcsany.minecraft.ervin_mod_1.world.biomes;
+
+import com.babcsany.minecraft.ervin_mod_1.init.ModBiomeFeatures;
+import com.babcsany.minecraft.ervin_mod_1.world.feature.FirgTree;
+import com.babcsany.minecraft.ervin_mod_1.world.feature.ModDefaultBiomeFeatures;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.carver.WorldCarver;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.ProbabilityConfig;
+import net.minecraft.world.gen.feature.TwoFeatureChoiceConfig;
+import net.minecraft.world.gen.placement.*;
+
+public class ExampleBiome2 extends Biome {
+
+	public ExampleBiome2(Builder biomeBuilder) {
+		super(biomeBuilder);
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.ZOMBIE, 10, 20, 50));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.SPIDER, 40, 20, 50));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.VILLAGER, 25, 20, 50));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.WANDERING_TRADER, 30, 15, 50));
+		this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityType.BAT, 20, 22, 58));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.BLAZE, 5, 20, 50));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.COMMAND_BLOCK_MINECART, 15, 20, 50));
+		this.addSpawn(EntityClassification.WATER_CREATURE, new SpawnListEntry(EntityType.DOLPHIN, 20, 20, 50));
+		this.addSpawn(EntityClassification.MISC, new SpawnListEntry(EntityType.ARROW, 25, 20, 50));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.CAT, 30, 20, 50));
+
+		//this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityInit.ZUR_ENTITY.get(), 5, 5, 20));
+		this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityType.BEE, 20, 2, 10));
+		//this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityInit.ZUR_ENTITY1.get(), 15, 10, 30));
+		//this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityInit.VILT_ENTITY.get(), 30, 15, 40));
+		//this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityInit.SRACH_ENTITY.get(), 8, 8, 16));
+		this.addCarver(GenerationStage.Carving.AIR,
+				Biome.createCarver(WorldCarver.UNDERWATER_CAVE, new ProbabilityConfig(4.14285715F)));
+		this.addCarver(GenerationStage.Carving.AIR,
+				Biome.createCarver(WorldCarver.HELL_CAVE, new ProbabilityConfig(16.02F)));
+		this.addCarver(GenerationStage.Carving.AIR,
+				Biome.createCarver(WorldCarver.CANYON, new ProbabilityConfig(60.7421563921F)));
+		this.addCarver(GenerationStage.Carving.AIR,
+				Biome.createCarver(WorldCarver.UNDERWATER_CANYON, new ProbabilityConfig(12.02F)));
+		this.addCarver(GenerationStage.Carving.AIR,
+				Biome.createCarver(WorldCarver.CAVE, new ProbabilityConfig(20.7421563921F)));
+		this.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.FOSSIL.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
+						.withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(128))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.FANCY_TREE.withConfiguration(ModDefaultBiomeFeatures.FIRG_TREE_CONFIG3)
+						.withPlacement(Placement.CHANCE_HEIGHTMAP_DOUBLE.configure(new ChanceConfig(240))));
+		this.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.FANCY_TREE.withConfiguration(ModDefaultBiomeFeatures.FIRG_TREE_CONFIG2)
+						.withPlacement(Placement.CHANCE_HEIGHTMAP.configure(new ChanceConfig(150))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.FANCY_TREE.withConfiguration(ModDefaultBiomeFeatures.FIRG_TREE_CONFIG1)
+						.withPlacement(Placement.CHANCE_TOP_SOLID_HEIGHTMAP.configure(new ChanceConfig(200))));
+		this.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.FANCY_TREE.withConfiguration(ModDefaultBiomeFeatures.FIRG_TREE_CONFIG)
+						.withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(100))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_BOOLEAN_SELECTOR
+						.withConfiguration(new TwoFeatureChoiceConfig(
+								Feature.HUGE_RED_MUSHROOM.withConfiguration(DefaultBiomeFeatures.BIG_RED_MUSHROOM),
+								Feature.HUGE_BROWN_MUSHROOM.withConfiguration(DefaultBiomeFeatures.BIG_BROWN_MUSHROOM)))
+						.withPlacement(Placement.COUNT_HEIGHTMAP.configure(new FrequencyConfig(1))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_BOOLEAN_SELECTOR
+						.withConfiguration(new TwoFeatureChoiceConfig(
+								Feature.FANCY_TREE.withConfiguration(ModDefaultBiomeFeatures.field_230136_t_),
+								Feature.ACACIA_TREE.withConfiguration(ModDefaultBiomeFeatures.FRIM_TREE_CONFIG)))
+						.withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(1))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.BROWN_MUSHROOM_CONFIG).withPlacement(
+						Placement.COUNT_CHANCE_HEIGHTMAP.configure(new HeightWithChanceConfig(1, 0.25F))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.RED_MUSHROOM_CONFIG).withPlacement(
+						Placement.COUNT_CHANCE_HEIGHTMAP_DOUBLE.configure(new HeightWithChanceConfig(1, 0.125F))));
+		this.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES,
+				Feature.ACACIA_TREE.withConfiguration(ModDefaultBiomeFeatures.FIRG_TREE_WITH_MORE_BEEHIVES_CONFIG1).withPlacement(
+						Placement.CHANCE_TOP_SOLID_HEIGHTMAP.configure(new ChanceConfig(250))));
+		//this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+		//		Feature.NORMAL_TREE.withConfiguration(JazzTree.JAZZ_TREE_CONFIG).withPlacement(
+		//				Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(7, 0.1f, 1))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.FANCY_TREE.withConfiguration(FirgTree.FIRG_TREE_CONFIG).withPlacement(
+						Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(18, 4.8f, 10))));
+
+		DefaultBiomeFeatures.addOres(this);
+		DefaultBiomeFeatures.addExtraGoldOre(this);
+		ModDefaultBiomeFeatures.addExtraEmeraldOre(this);
+		ModBiomeFeatures.addExampleFeature(this, 1000);
+		ModDefaultBiomeFeatures.addKiomne(this);
+	}
+
+	private static <T extends Entity> EntityType<T> register(String key, EntityType.Builder<T> builder) {
+		return Registry.register(Registry.ENTITY_TYPE, key, builder.build(key));
+	}
+	
+	@Override
+	public int getGrassColor(double posX, double posZ) {
+		return 0x00FF80;
+	}
+}
+
+//update mappings post command:
+//gradlew --refresh-dependencies
---
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/world/biomes/ExampleBiome4.java
@@ -0,0 +1,105 @@
+package com.babcsany.minecraft.ervin_mod_1.world.biomes;
+
+import com.babcsany.minecraft.ervin_mod_1.init.ModBiomeFeatures;
+import com.babcsany.minecraft.ervin_mod_1.world.feature.FirgTree;
+import com.babcsany.minecraft.ervin_mod_1.world.feature.ModDefaultBiomeFeatures;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.TwoFeatureChoiceConfig;
+import net.minecraft.world.gen.placement.*;
+
+public class ExampleBiome4 extends Biome {
+
+	public ExampleBiome4(Builder biomeBuilder) {
+		super(biomeBuilder);
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.ZOMBIE, 10, 2, 5));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.IRON_GOLEM, 50, 4, 50));
+		this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityType.BEE, 20, 2, 10));
+		this.addSpawn(EntityClassification.MONSTER, new SpawnListEntry(EntityType.SKELETON, 40, 10, 20));
+		this.addSpawn(EntityClassification.CREATURE, new SpawnListEntry(EntityType.BAT, 30, 20, 40));
+		this.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.FOSSIL.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
+						.withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(5120))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.MEGA_JUNGLE_TREE.withConfiguration(ModDefaultBiomeFeatures.MEGA_FIRG_TREE_CONFIG)
+						.withPlacement(Placement.CHANCE_HEIGHTMAP_DOUBLE.configure(new ChanceConfig(24))));
+		this.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.END_CITY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
+						.withPlacement(Placement.CHANCE_HEIGHTMAP.configure(new ChanceConfig(15))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.NETHER_BRIDGE.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
+						.withPlacement(Placement.CHANCE_TOP_SOLID_HEIGHTMAP.configure(new ChanceConfig(20))));
+		this.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.RANDOM_PATCH.withConfiguration(ModDefaultBiomeFeatures.GRASS_CONFIG)
+						.withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(1000))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_BOOLEAN_SELECTOR
+						.withConfiguration(new TwoFeatureChoiceConfig(
+								Feature.HUGE_RED_MUSHROOM.withConfiguration(DefaultBiomeFeatures.BIG_RED_MUSHROOM),
+								Feature.HUGE_BROWN_MUSHROOM.withConfiguration(DefaultBiomeFeatures.BIG_BROWN_MUSHROOM)))
+						.withPlacement(Placement.COUNT_HEIGHTMAP.configure(new FrequencyConfig(1))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_BOOLEAN_SELECTOR
+						.withConfiguration(new TwoFeatureChoiceConfig(
+								Feature.HUGE_RED_MUSHROOM.withConfiguration(ModDefaultBiomeFeatures.BIG_BROWN_MUSHROOM),
+								Feature.ACACIA_TREE.withConfiguration(ModDefaultBiomeFeatures.FRIM_TREE_CONFIG)))
+						.withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(1))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.BROWN_MUSHROOM_CONFIG).withPlacement(
+						Placement.COUNT_CHANCE_HEIGHTMAP.configure(new HeightWithChanceConfig(1, 0.25F))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.RED_MUSHROOM_CONFIG).withPlacement(
+						Placement.COUNT_CHANCE_HEIGHTMAP_DOUBLE.configure(new HeightWithChanceConfig(1, 0.125F))));
+		this.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES,
+				Feature.ACACIA_TREE.withConfiguration(ModDefaultBiomeFeatures.FANCY_TREE_WITH_MORE_BEEHIVES_CONFIG).withPlacement(
+						Placement.CHANCE_TOP_SOLID_HEIGHTMAP.configure(new ChanceConfig(25))));
+		//this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+		//		Feature.NORMAL_TREE.withConfiguration(JazzTree.JAZZ_TREE_CONFIG).withPlacement(
+		//				Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(7, 0.1f, 1))));
+		this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
+				Feature.FANCY_TREE.withConfiguration(FirgTree.FIRG_TREE_CONFIG).withPlacement(
+						Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(18, 4.8f, 10))));
+
+		DefaultBiomeFeatures.addOres(this);
+		DefaultBiomeFeatures.addExtraGoldOre(this);
+		ModDefaultBiomeFeatures.addExtraEmeraldOre(this);
+		ModBiomeFeatures.addExampleFeature(this, 10000);
+		ModDefaultBiomeFeatures.addExtraVilktBlock(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock1(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock2(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock3(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock4(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock5(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock6(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock7(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock8(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock9(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock10(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock11(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock12(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock13(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock14(this);
+		ModDefaultBiomeFeatures.addExtraVilktBlock15(this);
+		ModDefaultBiomeFeatures.addKiomne(this);
+		ModDefaultBiomeFeatures.addEndCity(this);
+	}
+
+	private static <T extends Entity> EntityType<T> register(String key, EntityType.Builder<T> builder) {
+		return Registry.register(Registry.ENTITY_TYPE, key, builder.build(key));
+	}
+	
+	@Override
+	public int getGrassColor(double posX, double posZ) {
+		return 0xFF0000;
+	}
+}
+
+//update mappings post command:
+//gradlew --refresh-dependencies
