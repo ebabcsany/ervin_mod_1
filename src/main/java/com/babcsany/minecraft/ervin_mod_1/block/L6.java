dea750b4643a750ea06a5f0bb7964aa2cd0081dd
Ervin Babcsány <ervin@babcsany.com>
2020-09-08 07:25:19 +0200

    11 modified ervin_mod_1/block, L0.java, L1.java, L2.java, L3.java, L4.java, L5.java, L6.java, L8.java, L9.java, RedFurance.java

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/block/L6.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/block/L6.java
@@ -1,7 +1,22 @@
 package com.babcsany.minecraft.ervin_mod_1.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+
+import static net.minecraft.block.HorizontalBlock.HORIZONTAL_FACING;
 
 public class L6 extends Block {
-    public L6(Block.Properties properties) { super(properties); }
+    public L6(Properties properties) {
+        super(properties);
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(HORIZONTAL_FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
 }
