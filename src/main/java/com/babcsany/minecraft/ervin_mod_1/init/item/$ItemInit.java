d8f89351ad7fb28fa3bbfa5f598fdea2c2087a40
Ervin Babcsány <ervin@babcsany.com>
2024-01-15 12:25:16 +0100

    latest1 -_ version-1 (1.16.1), after fixing bugs(24) ($ to USD)

--- src/main/java/com/babcsany/minecraft/ervin_mod_1/init/item/$ItemInit.java
+++ src/main/java/com/babcsany/minecraft/ervin_mod_1/init/item/$ItemInit.java
@@ -2,7 +2,6 @@ package com.babcsany.minecraft.ervin_mod_1.init.item;
 
 import com.babcsany.minecraft.ervin_mod_1.Ervin_mod_1;
 import com.babcsany.minecraft.ervin_mod_1.item.*;
-import com.babcsany.minecraft.ervin_mod_1.item.food.Foods;
 import com.babcsany.minecraft.ervin_mod_1.item.group.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -13,14 +12,14 @@ public class $ItemInit {
 
     public static final DeferredRegister<Item> $ = DeferredRegister.create(ForgeRegistries.ITEMS, Ervin_mod_1.MOD_ID);
 
-    public static final RegistryObject<Item> $_1$ = $.register("$/1$", () -> new $_1$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_2$ = $.register("$/2$", () -> new $_2$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_3$ = $.register("$/3$", () -> new $_3$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_4$ = $.register("$/4$", () -> new $_4$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_5$ = $.register("$/5$", () -> new $_5$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_6$ = $.register("$/6$", () -> new $_6$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_7$ = $.register("$/7$", () -> new $_7$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_8$ = $.register("$/8$", () -> new $_8$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_9$ = $.register("$/9$", () -> new $_9$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
-    public static final RegistryObject<Item> $_10$ = $.register("$/10$", () -> new $_10$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_1$ = $.register("USD/1", () -> new $_1$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_2$ = $.register("USD/2", () -> new $_2$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_3$ = $.register("USD/3", () -> new $_3$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_4$ = $.register("USD/4", () -> new $_4$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_5$ = $.register("USD/5", () -> new $_5$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_6$ = $.register("USD/6", () -> new $_6$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_7$ = $.register("USD/7", () -> new $_7$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_8$ = $.register("USD/8", () -> new $_8$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_9$ = $.register("USD/9", () -> new $_9$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
+    public static final RegistryObject<Item> $_10$ = $.register("USD/10", () -> new $_10$((new Item.Properties()).isBurnable().group(ItemGroup.ERVIN_MOD_1_ITEMS)));
 }
